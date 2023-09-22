f2fc73c7e773005e3be5893464477a00e72b945f
elieldiazgalindo <eliel.diazgalindo@utxicotepec.edu.mx>
2023-09-22 12:34:46 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/dmi_tarea7_200327/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dmi_tarea7_200327
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+// Creado por: Eliel Diaz Galindo
+// Asignatura: Desarrollo Movil Integral
+//Grado: 10   Grupo: "A"
+// Docente: MTI. Marco Antonio Ramirez Hernandez
+import 'package:flutter/material.dart';
+import 'package:dmi_tarea7_200327/src/button.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyButton(),
+  ));
+}
